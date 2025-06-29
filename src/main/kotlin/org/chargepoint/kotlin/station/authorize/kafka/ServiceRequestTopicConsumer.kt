40c2c8474974a58a436ce393116ca674a161d501
Heshawa De Silva <sharindu@accelaero.com>
2025-06-29 23:33:45 +0400

    Implementation: Read topic name and group Id from application.properties

--- src/main/kotlin/org/chargepoint/kotlin/station/authorize/kafka/ServiceRequestTopicConsumer.kt
+++ src/main/kotlin/org/chargepoint/kotlin/station/authorize/kafka/ServiceRequestTopicConsumer.kt
@@ -3,16 +3,21 @@ package org.chargepoint.kotlin.station.authorize.kafka
 import org.chargepoint.kotlin.station.authorize.dto.ServiceRequestContext
 import org.chargepoint.kotlin.station.authorize.service.StationAuthorizationService
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.KafkaListener
 
 @Configuration
 class ServiceRequestTopicConsumer(
-    private val stationAuthorizationService: StationAuthorizationService
+    private val stationAuthorizationService: StationAuthorizationService,
+    @Value("\${kafka.topics.charge-point-server-request}")
+    val consumerTopicName : String,
+    @Value("\${spring.kafka.consumer.group.id}")
+    val consumerGroupId : String
 ) {
     var log = LoggerFactory.getLogger(ServiceRequestTopicConsumer::class.java)
 
-    @KafkaListener(topics = ["charge-point-service-request"], groupId = "my-consumer-group")
+    @KafkaListener(topics = ["#{__listener.consumerTopicName}"], groupId = "#{__listener.consumerGroupId}")
     fun readTopicMessages(message : ServiceRequestContext){
         log.info("Message received from topic. Message Id: ${message.requestCorrelationId}")
         stationAuthorizationService.processMessagesFromKafka(message)
